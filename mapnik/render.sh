7f94006741c2c1583f9c422359e3b8af00ae57ee
philins <philins@ya.ru>
2020-11-03 20:33:25 +0300

    add files

--- mapnik/render.sh
+++ mapnik/render.sh
@@ -3,5 +3,4 @@ python prepare.py
 python3 generate_t.py
 mkdir ~/.ssh
 ssh-keyscan -H $FTPHOST >> ~/.ssh/known_hosts 
-sshpass -p $FTPPASS rsync --archive --verbose --progress tiles/ $FTPUSER@$FTPHOST:/storage/www/moscowmap/moscowmap.ru/htdocs/leafletnew/tiles/
-python -m SimpleHTTPServer 8080
+sshpass -p $FTPPASS rsync --archive tiles/ $FTPUSER@$FTPHOST:/storage/www/moscowmap/moscowmap.ru/htdocs/leafletnew/tiles/
